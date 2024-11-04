f2dc0959c6366cfdec6749912ae6573adc86800d
ElSeoClub <elseoclub@gmail.com>
2024-11-04 00:27:39 -0600

    Change ids to uuids

--- src/Database/Migrations/2024_10_27_094304_create_model_files_table.php
+++ src/Database/Migrations/2024_10_27_094304_create_model_files_table.php
@@ -4,24 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    public function up(): void
-    {
-        Schema::create('model_files', function (Blueprint $table) {
-            $table->id();
-            $table->morphs('fileable');
-            $table->string('type');
-            $table->string('name');
-            $table->string('extension');
-            $table->string('storage')->default('local');
-            $table->string('path');
+return new class extends Migration {
+    public function up(): void {
+        Schema::create( 'model_files', function ( Blueprint $table ) {
+            $table->uuid( 'id' )->primary();
+            $table->morphs( 'fileable' );
+            $table->string( 'type' );
+            $table->string( 'name' );
+            $table->string( 'extension' );
+            $table->string( 'storage' )->default( 'local' );
+            $table->string( 'path' );
             $table->timestamps();
-        });
+        } );
     }
 
-    public function down(): void
-    {
-        Schema::dropIfExists('model_files');
+    public function down(): void {
+        Schema::dropIfExists( 'model_files' );
     }
 };
--- src/Models/ModelFile.php
+++ src/Models/ModelFile.php
@@ -4,10 +4,26 @@ namespace Elseoclub\Modelfiler\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
-use InvalidArgumentException;
+use Illuminate\Support\Str;
 
 class ModelFile extends Model {
     protected $table = 'model_files';
+
+    public $incrementing = false;
+
+    // Especifica el tipo de clave primaria
+    protected $keyType = 'string';
+
+    protected static function boot() {
+        parent::boot();
+
+        // Genera un UUID antes de crear el registro si no se ha definido un ID
+        static::creating( function ( $model ) {
+            if ( empty( $model->id ) ) {
+                $model->id = (string) Str::uuid();
+            }
+        } );
+    }
 
     protected $fillable = [
         'fileable_id',
